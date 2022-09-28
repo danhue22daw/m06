71295cd49ba44c1afe895c7edf2052f2b1fda9eb
danhue22daw <15584975.clot@fje.edu>
2022-09-28 17:28:47 +0200

    Exercici 1.2

--- exercici1.2.js
+++ exercici1.2.js
@@ -1,53 +1,64 @@
+let win = 0,lose=0,tie=0;
 window.onload= function(){
     let respostesValides=["pedra","paper","tisores"];
     let start = document.getElementById('start');
-    let oponent=respostesValides[Math.floor(Math.random() * 3)];
+    let cmpt = document.getElementById('cmpt');
+    
     document.getElementById('pedra').onclick = function(){
-        console.log('boto');
+        let oponent=respostesValides[Math.floor(Math.random() * 3)];
         let resposta = "pedra";
         document.getElementById('ans').innerHTML=`Has seleccionat ${resposta}.<br>`;
         document.getElementById('ans').innerHTML+=`El teu oponent ha seleccionat ${oponent}.<br>`;
         if(oponent == 'pedra'){
             document.getElementById('ans').innerHTML+='Heu empatat!';
+            tie+=1;
         }
         else if(oponent=='paper'){
             document.getElementById('ans').innerHTML+='Has perdut!';
+            lose+=1;
         }
         else{
             document.getElementById('ans').innerHTML+='Has guanyat!';
+            win+=1;
         }
-        start.style.display="none";
+        cmpt.innerHTML=`<br><br>Partides guanyades: ${win}<br>Partides perdudes: ${lose}<br>Partides empatades: ${tie}`;
     }
     document.getElementById('paper').onclick = function(){
-        console.log('boto');
         let resposta = "paper";
+        let oponent=respostesValides[Math.floor(Math.random() * 3)];
         document.getElementById('ans').innerHTML=`Has seleccionat ${resposta}.<br>`;
         document.getElementById('ans').innerHTML+=`El teu oponent ha seleccionat ${oponent}.<br>`;
         if(oponent == 'pedra'){
             document.getElementById('ans').innerHTML+='Has guanyat!';
+            win++;
         }
         else if(oponent=='paper'){
             document.getElementById('ans').innerHTML+='Heu empatat!';
+            tie++;
         }
         else{
             document.getElementById('ans').innerHTML+='Has perdut!';
+            lose++;
         }
-        start.style.display="none";
+        cmpt.innerHTML=`<br><br>Partides guanyades: ${win}<br>Partides perdudes: ${lose}<br>Partides empatades: ${tie}`;
     }
     document.getElementById('tisores').onclick = function(){
-        console.log('boto');
+        let oponent=respostesValides[Math.floor(Math.random() * 3)];
         let resposta = "tisores";
         document.getElementById('ans').innerHTML=`Has seleccionat ${resposta}.<br>`;
         document.getElementById('ans').innerHTML+=`El teu oponent ha seleccionat ${oponent}.<br>`;
         if(oponent == 'pedra'){
             document.getElementById('ans').innerHTML+='Has perdut!';
+            lose++;
         }
         else if(oponent=='paper'){
             document.getElementById('ans').innerHTML+='Has guanyat!';
+            win++;
         }
         else{
             document.getElementById('ans').innerHTML+='Heu empatat!';
+            tie++;
         }
-        start.style.display="none";
+        cmpt.innerHTML=`<br><br>Partides guanyades: ${win}<br>Partides perdudes: ${lose}<br>Partides empatades: ${tie}`;
     }
 }
